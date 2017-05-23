5dd3993b5136edc8fbad4836d6aaec6ac92e449c
mak001 <mak001001@gmail.com>
2017-05-23 06:28:40 -0500

    Moved some code

--- mysite/_config.php
+++ mysite/_config.php
@@ -3,17 +3,17 @@
 global $project;
 $project = 'mysite';
 
-global $databaseConfig;
-$databaseConfig = array(
-	'type' => 'MySQLDatabase',
-	'server' => 'localhost',
-	'username' => 'root',
-	'password' => '',
-	'database' => 'SS_portfolio',
-	'path' => ''
-);
+global $database;
+$database = '';
+require_once('conf/ConfigureFromEnv.php');
 
 // Set the site locale
 i18n::set_locale('en_US');
+
+if (Director::isDev()) {
+    SS_Log::add_writer(new SS_LogFileWriter('../ss_error-warning.log', SS_Log::WARN, '<='));
+    SS_Log::add_writer(new SS_LogFileWriter('../ss_error.log', SS_Log::ERR));
+    SS_Log::add_writer(new SS_LogFileWriter('../ss_notices.log', SS_Log::NOTICE));
+}
 
 ?>
