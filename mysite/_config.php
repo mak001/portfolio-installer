2a60c9dc290b31798ffce67038356b8a71a8fbf5
mak001 <mak001001@gmail.com>
2017-05-22 19:55:43 -0500

    Finished silverstripe installation

--- mysite/_config.php
+++ mysite/_config.php
@@ -3,10 +3,30 @@
 global $project;
 $project = 'mysite';
 
-global $database;
-$database = '';
-
-require_once('conf/ConfigureFromEnv.php');
+global $databaseConfig;
+$databaseConfig = array(
+	'type' => 'MySQLDatabase',
+	'server' => 'localhost',
+	'username' => 'root',
+	'password' => '',
+	'database' => 'SS_portfolio',
+	'path' => ''
+);
 
 // Set the site locale
 i18n::set_locale('en_US');
+
+BreadcrumbNavigation::$includeHome = false;
+BreadcrumbNavigation::$includeSelf = true; 
+BreadcrumbNavigation::$maxDepth = 20; 
+BreadcrumbNavigation::$stopAtPageType = false; 
+BreadcrumbNavigation::$showHidden = false; 
+BreadcrumbNavigation::$homeURLSegment = 'home';
+
+if (!Director::isDev()) {
+    SS_Log::add_writer(new SS_LogFileWriter('../ss_error-warning.log', SS_Log::WARN, '<='));
+    SS_Log::add_writer(new SS_LogFileWriter('../ss_error.log', SS_Log::ERR));
+    SS_Log::add_writer(new SS_LogFileWriter('../ss_notices.log', SS_Log::NOTICE));
+}
+
+?>
