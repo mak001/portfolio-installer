c955d2e03b0895ab52f8e5a772e3e9698e9c57aa
mak001 <mak001001@gmail.com>
2021-08-24 09:19:25 -0500

    Added fixture file

---
+++ app/tests/Model/LinkTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Mak001\Portfolio\Test\Model;
+
+use Mak001\Portfolio\Model\Link;
+use SilverStripe\Dev\SapphireTest;
+use SilverStripe\Forms\FieldList;
+use SilverStripe\Forms\HiddenField;
+use SilverStripe\ORM\ValidationException;
+
+class LinkTest extends SapphireTest
+{
+
+    /**
+     * @var string
+     */
+    protected static $fixture_file = "../fixtures.yml";
+
+    /**
+     *
+     */
+    public function testGetCMSFields()
+    {
+        $object = $this->objFromFixture(Link::class, 'google');
+        $this->assertInstanceOf(FieldList::class, $object->getCMSFields());
+    }
+
+    /**
+     *
+     */
+    public function testGetCMSValidator()
+    {
+        $object = $this->objFromFixture(Link::class, 'google');
+        $object->Title = '';
+        $this->expectException(ValidationException::class);
+        $object->write();
+    }
+}
