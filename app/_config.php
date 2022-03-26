e89a930a7779c33cc1166fd7bd2fcf87815dcfe9
mak001 <mak001001@gmail.com>
2022-03-26 07:59:02 -0500

    composer2 support, fixed first build error

--- app/_config.php
+++ app/_config.php
@@ -7,6 +7,8 @@ use Wilr\GoogleSitemaps\GoogleSitemap;
 use Mak001\Portfolio\Model\Project\Framework;
 use Mak001\Portfolio\Model\Project\Language;
 use Mak001\Portfolio\Page\ProjectHolder;
+use SilverStripe\Core\Config\Config;
+use SilverStripe\ORM\DB;
 
 // remove PasswordValidator for SilverStripe 5.0
 $validator = PasswordValidator::create();
@@ -16,11 +18,14 @@ Member::set_password_validator($validator);
 GoogleSitemap::register_dataobject(Framework::class);
 GoogleSitemap::register_dataobject(Language::class);
 
-/** @var DataList|ProjectHolder[] $projectHolder */
-$projectHolders = ProjectHolder::get();
-$routes = [];
-foreach ($projectHolders as $holder) {
-    $routes[] = $holder->LanguageLink();
-    $routes[] = $holder->FrameworkLink();
+$tables = DB::table_list();
+if (count($tables) && array_key_exists(Config::inst()->get(ProjectHolder::class, 'table_name'), $tables)) {
+    /** @var DataList|ProjectHolder[] $projectHolder */
+    $projectHolders = ProjectHolder::get();
+    $routes = [];
+    foreach ($projectHolders as $holder) {
+        $routes[] = $holder->LanguageLink();
+        $routes[] = $holder->FrameworkLink();
+    }
+    GoogleSitemap::register_routes($routes);
 }
-GoogleSitemap::register_routes($routes);
