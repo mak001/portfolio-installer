76551875fc9f5029040c7abc2ca66d0a0391e8c8
mak001 <mak001001@gmail.com>
2022-07-31 07:02:33 -0500

    fixed size media queries breaking on most browsers

--- themes/portfolio-openprops/gulpfile.js
+++ themes/portfolio-openprops/gulpfile.js
@@ -9,6 +9,7 @@ const sourcemaps = require('gulp-sourcemaps');
 const sass = require('gulp-sass')(require('sass'));
 const postcss = require('gulp-postcss');
 const postcssCustomMedia = require('postcss-custom-media');
+var minmax = require('postcss-media-minmax');
 const shortCssVars = require('postcss-short-css-vars');
 const fs = require('fs');
 const uglifycss = require('gulp-uglifycss');
@@ -53,7 +54,7 @@ gulp.task('open-props', gulp.series('clean-open-props', 'copy-open-props'));
 gulp.task('scss', function () {
     const sassOptions = devBuild ? { outputStyle: 'expanded' } : { outputStyle: 'compressed' };
 
-    const postcssPlugins = [postcssCustomMedia()];
+    const postcssPlugins = [postcssCustomMedia(), minmax()];
     if (!devBuild) {
         postcssPlugins.push(shortCssVars());
     }
